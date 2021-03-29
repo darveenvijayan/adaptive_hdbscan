01143410d1c6318fe313a6e26a27ffdc4070787e
Darveen Vijayan <43519646+darveenvijayan@users.noreply.github.com>
2021-03-29 16:13:01 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,33 @@
 
-#rm -R venv
+# rm -R venv
 
-#python3 -m venv venv
+# python3 -m venv venv
+# source venv/bin/activate
+
+# cd hdbscan
+# pip install --no-cache-dir -r requirements.txt
+# python setup.py install
+
+# cd ..
+# python hdbscan_babysit.py
+
+
+
+######################### TAKE 2 ##########################################
+# source venv/bin/activate
+# cd hdbscan
+# python my_setup.py install
+# cd ..
+# python hdbscan_babysit.py
+
+# rm -R venv
+
+# python3 -m venv venv
 source venv/bin/activate
 
 cd hdbscan
-pip install --no-cache-dir -r requirements.txt
-python setup.py install
+# pip install --no-cache-dir -r requirements.txt
+python my_setup.py install
 
 cd ..
 python hdbscan_babysit.py
